42a77972addddbd2b4ff9125df6c0d3572771ae6
jasonpieroni <jasonpieroni@gmail.com>
2023-06-08 18:31:51 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from './NavBar'
+import Body from './Body'
+
+function App() {
+  return (
+    <div className="container">
+      <NavBar />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <div>
+            <img src='public/fenway-park02-result.jpg' className='body-image'/>
+        </div>
+    )
+}
+
+export default Body;
