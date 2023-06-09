ef007b7982b8c43f2e5c6b3a7e3da8d35aa3540b
jasonpieroni <jasonpieroni@gmail.com>
2023-06-09 19:47:43 -0400

    new branch with player selector file

---
+++ Link.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+export default function Link() {
+    const [player, setPlayer] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleChange = (e) => {
+        setPlayer(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setMessage(`You selected ${player}!`)
+    }
+    return (
+    <div>
+        <label htmlFor='players'>Select Your Favorite Red Sox Hitter</label>
+        <form onSubmit={handleSubmit}>
+        <select name='players' id='players' value={player} onChange={handleChange}>
+            <option value="Select Player">~ Select Player ~</option>
+            <option value='Connor Wong'>Connor Wong</option>
+            <option value='Tristan Casas'>Tristan Casas</option>
+            <option value='Enmanuel Valdez'>Enmanuel Valdez</option>
+            <option value='Kike Hernandez'>Kike Hernandez</option>
+            <option value='Rafael Devers'>Rafael Devers</option>
+            <option value='Masataka Yoshida'>Masataka Yoshida</option>
+            <option value='Jarren Duran'>Jarren Duran</option>
+            <option value='Alex Verdugo'>Alex Verdugo</option>
+            <option value='Justin Turner'>Justin Turner</option>
+        </select>
+        <button type='submit'>Submit</button>
+        </form>
+    </div>
+    )
+}
