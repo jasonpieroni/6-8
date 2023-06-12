4bce6529fcf643ee3114451b128a54221ced0a92
jasonpieroni <jasonpieroni@gmail.com>
2023-06-12 18:31:43 -0400

    updated Body.jsx

--- src/Body.jsx
+++ src/Body.jsx
@@ -4,6 +4,19 @@ const Body = () => {
     return (
         <div>
             <img src='public/fenway-park02-result.jpg' className='body-image'/>
+            <h1>Red Sox Lineup Last Night</h1>
+            <ul>
+                <li>1B: Triston Casas</li>
+                <li>2B: Kike Hernandez</li>
+                <li>SS: Pablo Reyes</li>
+                <li>3B: Rafael Devers</li>
+                <li>LF: Masataka Yoshida</li>
+                <li>CF: Jarren Duran</li>
+                <li>RF: Alex Verdugo</li>
+                <li>C: Reese McGuire</li>
+                <li>SP: Brayan Bello</li>
+
+            </ul>
         </div>
     )
 }
