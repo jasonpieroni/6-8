7b182fc7680bd751131e7365e8dd9fa515c80762
jasonpieroni <jasonpieroni@gmail.com>
2023-07-02 22:10:42 -0400

    added score of today's game

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <nav className='nav'>
           <img src="/public/boston-red-sox-logo-transparent.png" className="redsox-image" />
           <h1 className="nav-title">Red Sox Fan Page</h1>
-          <h3>Tonight's Score: Boston Red Sox: 5 Toronto Blue Jays: 0</h3>
+          <h3>Tonight's Score: Boston Red Sox: 5 Toronto Blue Jays: 4</h3>
         </nav>
       </header>
     )
