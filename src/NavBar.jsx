f38ef8bb62ae8fb7f0de0b6aa99569a7991bbe54
jasonpieroni <jasonpieroni@gmail.com>
2023-07-04 20:48:05 -0400

    updated schedule

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <nav className='nav'>
           <img src="/public/boston-red-sox-logo-transparent.png" className="redsox-image" />
           <h1 className="nav-title">Red Sox Fan Page</h1>
-          <h3>Tomorrow's Opponent: Texas Rangers</h3>
+          <h3>Tonight's Opponent: Texas Rangers</h3>
         </nav>
       </header>
     )
